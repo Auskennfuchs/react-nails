6dfbd3d8bd3d3168821d2968eb6f895dc66778f0
Auskennfuchs <micschilling@hotmail.com>
2019-11-17 22:49:51 +0100

    Updates

---
+++ precache-manifest.0e1e05bd7e0244a0ee090285ef030fbd.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18af697c811bd62639484aa4692b74dd",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e3fa2ac2ae016b916aba.bundle.js"
+  },
+  {
+    "url": "runtime~main.e3fa2ac2ae016b916aba.bundle.js"
+  },
+  {
+    "revision": "6bcdcaed6aa3ea736d7d",
+    "url": "vendors~main.css"
+  },
+  {
+    "url": "vendors~main.e3fa2ac2ae016b916aba.bundle.js"
+  }
+]);
