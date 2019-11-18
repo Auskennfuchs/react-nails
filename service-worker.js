32bdec677754ba854bbe1a70992f3b5e8865e117
Auskennfuchs <micschilling@hotmail.com>
2019-11-18 22:40:07 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.8cf9bfdc1cd9f47b434365cb7f2e34fe.js"
+  "precache-manifest.83e08f193f41776296c475bbe5a923c6.js"
 );
 
 self.addEventListener('message', (event) => {
