55af30580f7208eb1e947a73cd5d4459dbfda63a
Auskennfuchs <auskennfuchs@gmail.com>
2020-07-25 16:32:06 +0200

    fix imports

--- src/layout/Column.tsx
+++ src/layout/Column.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
-import { resolveSpace, resolveLineSpace, resolveJustifyItems, resolveAlignItems } from '../properties/PropertyResolver'
-import { SpacingProps, LineSpaceProps, AlignItemProps, JustifyItemProps } from '../properties/PropertyTypes'
+import { resolveSpace, resolveLineSpace, resolveJustifyItems, resolveAlignItems } from 'properties/PropertyResolver'
+import { SpacingProps, LineSpaceProps, AlignItemProps, JustifyItemProps } from 'properties/PropertyTypes'
 
 export interface ColumnProps extends SpacingProps, LineSpaceProps, AlignItemProps, JustifyItemProps {
 }
--- src/theme/ThemeProps.tsx
+++ src/theme/ThemeProps.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { ThemeContext } from "styled-components"
+import { ThemeContext } from 'styled-components'
 
 const ThemeProps = ({ children }: { children: (theme: object) => any }) => {
     const themeContext = useContext(ThemeContext)
